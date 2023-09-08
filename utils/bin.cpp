6c2e7aa3b099211b3f1a78bf49980ce939c3d035
Fabricio Correa <fabriciocorreac@pm.me>
2023-09-08 17:52:19 -0300

    Modify utils/bin.cpp

--- utils/bin.cpp
+++ utils/bin.cpp
@@ -4,8 +4,8 @@
 
 #include <stdio.h>
 
-void printBits(char palabra, bool new_line=true){
-  for (char i = 7; i >= 0; i--){
+void printBits(char palabra, bool new_line=true, int length=8){
+  for (char i = length-1; i >= 0; i--){
     printf("%d", (palabra >> i) & 1);
   }
   if (new_line) {
@@ -13,8 +13,8 @@ void printBits(char palabra, bool new_line=true){
   }
 }
 
-void printBits(short palabra, bool new_line=true) {
-  for (char i = 15; i >= 0; i--){
+void printBits(short palabra, bool new_line=true, int length=16) {
+  for (char i = length-1; i >= 0; i--){
     printf("%d", (palabra >> i) & 1);
   }
   if (new_line) {
@@ -22,8 +22,8 @@ void printBits(short palabra, bool new_line=true) {
   }
 }
 
-void printBits(int palabra, bool new_line=true) {
-  for (char i = 31; i >= 0; i--){
+void printBits(int palabra, bool new_line=true, int length=32) {
+  for (char i = length-1; i >= 0; i--){
     printf("%d", (palabra >> i) & 1);
   }
   if (new_line) {
@@ -31,13 +31,44 @@ void printBits(int palabra, bool new_line=true) {
   }
 }
 
-// Imprimir Signo | Exponente | Mantisa de un número en OF de 32 bits.
+
+// Overloads con el orden inverso de los parámetros.
+
+void printBits(char palabra, int length=8, bool new_line=true){
+  for (char i = length-1; i >= 0; i--){
+    printf("%d", (palabra >> i) & 1);
+  }
+  if (new_line) {
+    printf("\n");
+  }
+}
+
+void printBits(short palabra, int length=16, bool new_line=true) {
+  for (char i = length-1; i >= 0; i--){
+    printf("%d", (palabra >> i) & 1);
+  }
+  if (new_line) {
+    printf("\n");
+  }
+}
+
+void printBits(int palabra, int length=32, bool new_line=true) {
+  for (char i = length-1; i >= 0; i--){
+    printf("%d", (palabra >> i) & 1);
+  }
+  if (new_line) {
+    printf("\n");
+  }
+}
+
+
+// Imprimir Signo | Exponente | Mantisa de un número en PF de 32 bits.
 void printFP(int palabra, bool new_line=true) {
-    printBits(palabra >> 31, false);
+    printBits(palabra >> 31, 1, false);
     printf(" | ");
-    printBits(palabra >> 23 & 0xFF, false);
+    printBits(palabra >> 23 & 0xFF, 8, false);
     printf(" | ");
-    printBits(palabra & 0x7FFFFF, false);
+    printBits(palabra & 0x7FFFFF, 23, false);
     if (new_line){
         printf("\n");
     }
